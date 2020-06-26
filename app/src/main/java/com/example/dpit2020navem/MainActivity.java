aae9de5b077689823bdaaddd4357df2c1fc51927
iondragos <iondragos0909@yahoo.com>
2020-06-26 13:47:50 +0300

    .

--- app/src/main/java/com/example/dpit2020navem/MainActivity.java
+++ app/src/main/java/com/example/dpit2020navem/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.dpit2020navem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMain = findViewById(R.id.buttonMain);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(AccountManagementActivity.this, LoginActivity.class);
+                //startActivity(intent);
+
+            }
+        });
     }
 }
