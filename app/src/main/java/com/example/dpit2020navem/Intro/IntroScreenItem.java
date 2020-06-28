68a9fa7fc3afe5b31d19fb7da27f71256f0df64b
iondragos <iondragos0909@yahoo.com>
2020-06-28 22:39:14 +0300

    .

---
+++ app/src/main/java/com/example/dpit2020navem/Intro/IntroScreenItem.java
@@ -0,0 +1,18 @@
+package com.example.dpit2020navem.Intro;
+
+public class IntroScreenItem {
+
+    String title;
+
+    public IntroScreenItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
